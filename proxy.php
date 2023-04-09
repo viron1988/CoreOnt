ed7b2885577bc185e4e9e8c40b2e4176696b826a
viron1988 <46696413+viron1988@users.noreply.github.com>
2023-04-09 23:01:52 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -10,14 +10,14 @@ $url = 'http://44.208.130.22/sendemail.php';
 $headers = array('Content-Type: application/json');
 
 // Initialize a new cURL session
-$curl = curl_init();
+// Initialize a new cURL session
+$curl = curl_init($url . '?' . $postData);
 
 // Set the cURL options
-curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($curl, CURLOPT_POST, true);
-curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
+curl_setopt($curl, CURLOPT_HTTPGET, true);
 curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
 
 // Execute the cURL request and get the response
 $response = curl_exec($curl);
@@ -25,5 +25,9 @@ $response = curl_exec($curl);
 // Close the cURL session
 curl_close($curl);
 
-// Return the response to your website
-echo $response;
+// Save the response to a file
+file_put_contents('response.log', $response);
+
+// Return a success message to the client
+echo 'Request completed successfully.';
+?>
