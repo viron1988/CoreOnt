590341b6825012eb0c31a670b29d6e765386f3b4
viron1988 <46696413+viron1988@users.noreply.github.com>
2023-04-09 23:19:24 +0200

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -1,18 +1,28 @@
 <?php
-
-// Get the data from the POST request
-$postData = file_get_contents('php://input');
-
 // Set the URL of your AWS server
 $url = 'http://44.208.130.22/sendemail.php';
 
 // Set any headers that are required for your request (e.g. Content-Type)
 $headers = array('Content-Type: application/json');
 
+// Allow requests from any origin
+header('Access-Control-Allow-Origin: *');
+
+// Handle pre-flight requests
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type');
+    exit();
+}
+
+// Get the data from the POST request
+$postData = file_get_contents('php://input');
+
 // Initialize a new cURL session
-$curl = curl_init($url);
+$curl = curl_init();
 
 // Set the cURL options
+curl_setopt($curl, CURLOPT_URL, $url);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $postData);
@@ -27,6 +37,6 @@ curl_close($curl);
 // Save the response to a file
 file_put_contents('response.log', $response);
 
-// Return a success message to the client
-echo 'Request completed successfully.';
+// Return the response to the client
+echo $response;
 ?>
