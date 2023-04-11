4c388460ef7650a9ac65a18591e0592264faa573
viron1988 <46696413+viron1988@users.noreply.github.com>
2023-04-11 20:40:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,9 @@ function handleArticleClick(event) {
   
   // Get the URL of the article from the link's href attribute
   var articleUrl = event.target.getAttribute("href");
-
-  // Send an AJAX request to the server to log the article click
-  $.ajax({
-    url: "/log-click.php",
-    type: "POST",
-    data: { url: articleUrl },
-    success: function() {
-      // If the request was successful, navigate to the article URL
-      window.location = articleUrl;
-    },
-    error: function() {
-      // If there was an error with the request, log the error to the console
-      console.log("Error logging article click.");
-    }
-  });
+  
+  // Set the current page URL to the article URL
+  window.location = articleUrl;
 }
 
 // Get a reference to the article list element
@@ -30,3 +18,57 @@ var articleLinks = articleList.getElementsByTagName("a");
 for (var i = 0; i < articleLinks.length; i++) {
   articleLinks[i].addEventListener("click", handleArticleClick);
 }
+
+function sendEmail(name, email, message, senderName, senderEmail) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://api.sendinblue.com/v3/smtp/email');
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('api-key', process.env.MY_SENDINBLUE_KEY);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      console.log(xhr.response);
+    }
+  };
+  var body = {
+    "sender": {
+      "name": senderName,
+      "email": senderEmail
+    },
+    "to": [
+      {
+        "email": "gnrrobotics@gmail.com",
+        "name": "GnR Robotics"
+      }
+    ],
+    "htmlContent": message,
+    "subject": "Message from " + name,
+    "replyTo": {
+      "email": email,
+      "name": name
+    }
+  };
+  xhr.send(JSON.stringify(body));
+}
+
+var submitButton = document.querySelector('input[type="submit"]');
+submitButton.addEventListener('click', function(event) {
+  event.preventDefault();
+  var nameInput = document.getElementById('name');
+  var emailInput = document.getElementById('email');
+  var messageInput = document.getElementById('message');
+  var senderNameInput = document.getElementById('sender-name');
+  var senderEmailInput = document.getElementById('sender-email');
+  var name = nameInput.value;
+  var email = emailInput.value;
+  var message = messageInput.value;
+  var senderName = senderNameInput.value;
+  var senderEmail = senderEmailInput.value;
+  sendEmail(name, email, message, senderName, senderEmail);
+});
+
+const form = document.querySelector('#contact-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  // You can replace the URL below with the URL of your thank-you page
+  window.location.href = 'thank-you.html';
+});
