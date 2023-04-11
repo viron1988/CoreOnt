290e382517e15c9124b5406b63288c7bdb41ceb4
viron1988 <46696413+viron1988@users.noreply.github.com>
2023-04-11 22:42:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ function sendEmail(name, email, message) {
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://api.sendinblue.com/v3/smtp/email');
   xhr.setRequestHeader('Content-Type', 'application/json');
+  console.log(window.SENDINBLUE_API_KEY);
   xhr.setRequestHeader('api-key', window.SENDINBLUE_API_KEY);
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4) {
